93bdf65187251827397b688cbf6ab0fe1910d33c
WRakocki <wiktor.rakocki6@protonmail.com>
2025-01-10 19:49:28 +0100

    cleaning up

--- huffman-compressor.py
+++ huffman-compressor.py
@@ -24,16 +24,20 @@ class Node:
     
 
 class PriorityQueue:
+
   """
   Klasa reprezentująca kolejke priorytetową
   """
+
   def __init__(self):
       self.heap = []
 
   def heapify(self, index):
+
     """
     Utrzymuje własność kopca w drzewie, modyfikując jeśli to konieczne.
     """
+
     left = 2 * index + 1
     right = 2 * index + 2
     smallest = index
@@ -49,9 +53,11 @@ class PriorityQueue:
         self.heapify(smallest)
 
   def insert(self, node):
+
     """
     Wstawia węzeł do kopca, utrzymując własność kopca.
     """
+
     self.heap.append(node)
     index = len(self.heap) - 1
 
@@ -60,6 +66,7 @@ class PriorityQueue:
         index = (index - 1) // 2
 
   def extract_min(self):
+
     """
     Usuwa i zwraca węzeł z minimalną wartością z kopca.
     """
@@ -75,27 +82,48 @@ class PriorityQueue:
     return min_node
 
   def peek(self):
-      if self.is_empty():
-          raise IndexError("peek() called on empty heap")
-      return self.heap[0]
+
+    """
+    Zwraca węzeł z minimalną wartością z kopca, nie usuwając go.
+    """
+
+    if self.is_empty():
+        raise IndexError("peek() called on empty heap")
+    return self.heap[0]
 
   def is_empty(self):
-      return len(self.heap) == 0
+
+    """
+    Sprawdza, czy kopiec jest pusty.
+    """
+
+    return len(self.heap) == 0
 
   def size(self):
       return len(self.heap)
 
 class Compression:
   def __init__(self):
-      self.q = PriorityQueue()
+
+    """
+    Inicjalizuje obiekt klasy Compression, tworząc pustą kolejkę priorytetową.
+    """
+
+    self.q = PriorityQueue()
 
   def get_text(self, filename):
+
+    """
+    Czyta z pliku o podanej nazwie i zwraca zawartość tekstu.
+    """
+
     with open(filename, "r") as file:
       text = file.read()
 
     return text
 
   def count_occurrences(self, text):
+
     """
     Liczy wystąpienia każdego znaku w tekście i zwraca słownik.
     """
@@ -111,10 +139,12 @@ class Compression:
     
 
   def huffman(self, occurences):
+
     """
     Zwraca korzeń drzewa Huffmana utworzonego na podstawie
     słownika wystąpień znaków w tekście.
     """
+
     n = len(occurences)
 
     for c in occurences.keys():
@@ -131,9 +161,11 @@ class Compression:
     return self.q.extract_min()
 
   def generate_huffman_codes(self, root, current_code="", codes=None):
+
     """
     Rekurencyjnie generuje kody Huffmana w drzewie, zaczynając od korzenia.
     """
+
     if codes is None:
       codes = {}
 
@@ -150,12 +182,24 @@ class Compression:
     return codes
   
   def encode(self, text, codes):
+
+    """
+    Koduje tekst za pomocą kodów Huffmana.
+    """
+
     encoded_text = ""
     for char in text:
         encoded_text += codes[char]
     return encoded_text
 
   def pad_encooded_text(self, encoded_text):
+
+    """
+    Dodaje padding do zakodowanego tekstu, tak aby jego długość była
+    wielokrotnonością 8. Do tekstu dodawany jest prefiks - 8-bitową 
+    reprezentację wartości extra padding, którą dekoder musi usunąć przy dekodowaniu.
+    """
+
     extra_padding = 8 - (len(encoded_text) % 8)
     for i in range(extra_padding):
       encoded_text += "0"
@@ -165,7 +209,11 @@ class Compression:
     return encoded_text
   
   def get_byte_array(self, padded_encoded_text):
-    
+
+    """
+    Konwertuje zakodowany i skonwertowany tekst na listę bajtów.
+    """
+
     b = bytearray()
     for i in range(0, len(padded_encoded_text), 8):
       byte = padded_encoded_text[i:i+8]
@@ -174,6 +222,12 @@ class Compression:
   
   def compress(self, filename, output_filename):
 
+    """
+    Kompresuje plik o podanej nazwie i zapisuje do pliku o podanej nazwie.
+    W pliku wyjściowym zapisywany jest słownik kodów Huffmana w czytelnym formacie, a następnie
+    zakodowany tekst binarnie.
+    """
+    
     text = self.get_text(filename)
     occurences = self.count_occurrences(text)
     root = self.huffman(occurences)
@@ -188,6 +242,11 @@ class Compression:
       file.write(bytes(byte_array))
   
   def remove_padding(self, padded_encoded_text):
+
+    """
+    Usuwa padding z zakodowanego tekstu.
+    """
+    
     padded_info = padded_encoded_text[:8]
     extra_padding = int(padded_info, 2)
 
@@ -197,6 +256,11 @@ class Compression:
     return encoded_text
   
   def decode_text(self, encoded_text, codes):
+
+    """
+    Dekoduje zakodowany tekst przy pomocy słownika kodów Huffmana.
+    """
+
     current_code = ""
     decoded_text = ""
     reversed_codes = {v: k for k, v in codes.items()}
@@ -211,6 +275,11 @@ class Compression:
   
   def decompress(self, filename, output_filename):
     
+    """
+    Dekompresuje plik o podanej nazwie i zapisuje odkompresowany tekst
+    do pliku o podanej nazwie.
+    """
+
     with open(filename, "rb") as file:
       codes = eval(file.readline())
       byte_array = bytearray(file.read())
@@ -224,6 +293,14 @@ class Compression:
      
 
 def main():
+
+  """
+  Główna funkcja programu. Przyjmuje 3 argumenty z linii komend:
+  - 'c' lub 'd' - c dla kompresji, d dla dekomresji
+  - nazwa pliku wejściowego
+  - nazwa pliku wyjściowego
+  """
+
   if sys.argv[1] == 'c':
     c = Compression()
     c.compress(sys.argv[2], sys.argv[3])
